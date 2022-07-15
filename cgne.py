3b5a071569b772066b09fc582f6cd3f4f1bb0d6a
danielobmann <obmann.daniel@live.de>
2022-07-15 07:38:59 +0200

    update cgne and add examples

--- cgne.py
+++ cgne.py
@@ -38,18 +38,51 @@ class ConjugateGradientNormalEquation(nn.Module):
         return x0
 
 
-def CGNE_numpy(x0, A, rhs, niter=10):
+def CGNE_numpy(x0, A, rhs, niter=10, return_error=False):
     d0 = rhs - A @ x0
     p0 = A.T @ d0
     s0 = p0
+    if return_error:
+        err = [np.sqrt(np.sum(d0 ** 2))]
 
     for k in range(niter):
         q0 = A @ p0
-        alpha = np.sum(s0 ** 2) / np.sum(q0 ** 2) ** 2
+        alpha = np.sum(s0 ** 2) / np.sum(q0 ** 2)
         x0 = x0 + alpha * p0
         d0 = d0 - alpha * q0
+        if return_error:
+            err.append(np.sqrt(np.sum(d0 ** 2)))
         s1 = A.T @ d0
         beta = np.sum(s1 ** 2) / np.sum(s0 ** 2)
         p0 = s1 + beta * p0
         s0 = s1
+    if return_error:
+        return x0, err
     return x0
+
+
+"""
+Examples of CGNE and its convergence for matrices with different behaviour of singular values
+
+import matplotlib.pyplot as plt
+
+n = 256
+niter = 200
+
+A = np.diag(1. / np.arange(1, n+1))
+x0 = np.ones(n)
+xcg, err = CGNE_numpy(x0, A, rhs=np.zeros_like(x0), niter=niter, return_error=True)
+plt.semilogy(err)
+
+entries = [1 / i if i <= 200 else 0 for i in np.arange(1, n+1)]
+B = np.diag(entries)
+x0 = np.ones(n)
+xcg, err = CGNE_numpy(x0, B, rhs=np.zeros_like(x0), niter=niter, return_error=True)
+plt.semilogy(err)
+
+q = 0.95
+C = np.diag(q ** np.arange(0, n))
+x0 = np.ones(n)
+xcg, err = CGNE_numpy(x0, C, rhs=np.zeros_like(x0), niter=niter, return_error=True)
+plt.semilogy(err)
+"""
