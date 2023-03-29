d893d9b857bd6c69b2ee2a9fda18600a149308e9
danielobmann <obmann.daniel@live.de>
2023-03-29 08:18:19 +0200

    added simple example of sampling with mcmc

--- posterior-example.py
+++ posterior-example.py
@@ -52,3 +52,40 @@ plt.title("Likelihood")
 plt.figure()
 plt.imshow(np.flipud(likelihood * prior), extent=[xmin, xmax, xmin, xmax], cmap=cmap)
 plt.title("Posterior")
+
+"""
+Sampling of posterior using MCMC method. This is a really simple method to sample a given posterior distribution.
+With enough samples the histogram of the samples starts to look like to correct posterior as defined above.
+The advantage of this method is, that one can use these samples to calculate many different things, e.g. conditional
+expectation or use it to minimize some Bayesian cost function.
+"""
+
+
+def mcmc(posterior, gamma=1, n=1000, dim=1, burn_in_factor=0.3):
+    x = np.zeros(dim)
+    samples = []
+    burn_in = int(n * burn_in_factor)
+    for k in range(n):
+        pi_x = posterior(x)
+        y = x + np.random.normal(0, gamma, dim)
+        pi_y = posterior(y)
+        acceptance = np.minimum(1, pi_y / pi_x)
+        if k > burn_in:
+            if np.random.uniform(0, 1) <= acceptance:
+                x = y
+                samples.append(y)
+    return np.stack(samples)
+
+
+def posterior(x, y):
+    prior = np.exp(- np.sum(x ** 2) / (2 * rho ** 2))
+    res = (A @ x - y)
+    likelihood = np.exp(- np.sum(res ** 2) / (2 * sigma ** 2))
+    return likelihood * prior
+
+
+post = lambda x: posterior(x, y=yobs)
+samples = mcmc(post, dim=2, n=10 ** 6)  # Sampling takes some time
+plt.figure()
+plt.hist2d(samples[:, 0], samples[:, 1], range=[[xmin, xmax], [xmin, xmax]], cmap=cmap, bins=50)
+plt.title("Empirical posterior")
